72100e0c55bffb6aed8c1a46239c9c7c722658b3
神園巧真 <>
2022-09-29 09:43:51 +0900

    headerの背景がスクロールしたとき白背景になる設定

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const hero = new HeroSlider(".swiper");
   hero.start();
 });
+
+const header = document.querySelector(".header");
+
+const _navAnimation = function (el, inview) {
+  if (inview) {
+    header.classList.remove("triggered");
+  } else {
+    header.classList.add("triggered");
+  }
+};
+
+const so = new ScrollObserver(".nav-trigger", _navAnimation, { once: false });
