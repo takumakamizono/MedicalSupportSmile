46776ef317da2819d52e061fc5c6a868a9afaedd
神園巧真 <>
2022-10-02 11:59:29 +0900

    main.jsの記述の最適化
    mainクラスにまとめてそれぞれモジュールで初期化設定し読み込むようにしました。

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const hero = new HeroSlider(".swiper");
-  hero.start();
+  new Main();
 });
 
-const header = document.querySelector(".header");
-
-const _navAnimation = function (el, inview) {
-  if (inview) {
-    header.classList.remove("triggered");
-  } else {
-    header.classList.add("triggered");
+class Main {
+  constructor() {
+    this.header = document.querySelector(".header");
+    this.hero = new HeroSlider(".swiper");
+    this._observers = [];
+    this._init();
   }
-};
 
-const so = new ScrollObserver(".nav-trigger", _navAnimation, { once: false });
+  _toggleSlideAnimation(el, inview) {
+    if (inview) {
+      this.hero.start();
+    } else {
+      this.hero.stop();
+    }
+  }
+  _navAnimation(el, inview) {
+    if (inview) {
+      this.header.classList.remove("triggered");
+    } else {
+      this.header.classList.add("triggered");
+    }
+  }
+  _init() {
+    new MobileMenu();
+    this._scrollIint();
+  }
 
-new MobileMenu();
+  //pushでオブジェクトに格納する。
+  _scrollIint() {
+    this._observers.push(
+      new ScrollObserver(".nav-trigger", this._navAnimation.bind(this), {
+        once: false,
+      }),
+      new ScrollObserver(".swiper", this._toggleSlideAnimation.bind(this), {
+        once: false,
+      })
+    );
+    console.log(this._observers);
+  }
+}
